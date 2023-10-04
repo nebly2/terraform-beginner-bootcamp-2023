532b64fb9ce63403b96fe86e17496103169ba8ef
nebly2 <127355682+nebly2@users.noreply.github.com>
2023-10-04 16:01:13 -0500

    #23 bucket should no longer use random provider (#24)

--- main.tf
+++ main.tf
@@ -2,18 +2,11 @@
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 
-resource "random_string" "bucket_name" {
-  # Bucket Naming rules
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  length = 32
-  special = false
-  lower = true
-  upper = false
-}
 
 
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
   tags = {
     UserUuid = var.user_uuid
   }
