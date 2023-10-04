47e1c1e6673a2ea5a1cd38723f047d66201d7b26
Benjamin Ly <benjaminly123@gmail.com>
2023-10-04 22:16:04 +0000

    first commit

---
+++ modules/terrahouse_aws/main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
+
+terraform{
+    required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
