cd79415a1f7f4e32b16e273e363c603c2441e9a2
Benjamin Ly <benjaminly123@gmail.com>
2023-10-07 23:19:45 +0000

    #32 only update index.html if content_version changes

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -26,6 +26,10 @@ resource "aws_s3_object" "index_html" {
   content_type = "text/html"
 
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -35,6 +39,9 @@ resource "aws_s3_object" "error_html" {
   source = var.error_html_filepath
 
   etag = filemd5(var.error_html_filepath)
+  # lifecycle {
+  #   ignore_changes = [etag]
+  # }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -58,4 +65,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
