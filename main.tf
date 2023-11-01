fbd790b4cb878b720f82a1f8be6d6ff2cada8fdd
Benjamin Ly <benjaminly123@gmail.com>
2023-11-01 06:02:35 +0000

    #46 test terrahome on terratowns test town

--- main.tf
+++ main.tf
@@ -21,29 +21,29 @@ terraform {
   #}
 
 }
-
+// 2ff43ed9-3759-4dd6-978b-e6de3cc3d48a
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = "https://terratowns.cloud/api"
+  user_uuid=var.teacherseat_user_uuid
+  token= var.terratowns_access_token
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version = var.content_version
-#}
+module "terrahouse_aws" {
+ source = "./modules/terrahouse_aws"
+ user_uuid = var.teacherseat_user_uuid
+ index_html_filepath = var.index_html_filepath
+ error_html_filepath = var.error_html_filepath
+ content_version = var.content_version
+ assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
   name = "What team compositions to play in Teamfight Tactics (set 9.5)"
   description = <<DESCRIPTION
   Teamfight Tactics (TFT) is an auto-battler game mode within the popular multiplayer online battle arena (MOBA) game League of Legends, developed by Riot Games. In TFT, players assemble teams of champions and pit them against each other in automated battles. This guide will show you the team compositions that are the strongest in set 9.5.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  # domain_name = "dffs2gsh.cloudfront.net"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
   content_version = 1
 }
