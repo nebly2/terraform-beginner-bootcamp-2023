4b38fd2517cafb0be50ade3e36fdbb5af5f3d782
nebly2 <127355682+nebly2@users.noreply.github.com>
2023-11-06 12:06:51 -0600

    52 create html page (#53)
    * wip
    
    * #52 deployed brownies page to terratowns
    
    * #52 deployed brownies page to terratowns

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value = module.home_tft_hosting.bucket_name
+  value = module.home_brownies_hosting.bucket_name
 }
 
 output "s3_website_endpoint" {
   description = "S3 Static Website hosting endpoint"
-  value = module.home_tft_hosting.website_endpoint
+  value = module.home_brownies_hosting.website_endpoint
 }
 
 output "cloudfront_url" {
   description = "The CloudFront Distribution Domain Name"
-  value = module.home_tft_hosting.domain_name
+  value = module.home_brownies_hosting.domain_name
 }
